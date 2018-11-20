a8fcdc2105b62f4cd1f258e17e1cb04f88aae16a
Lubin1611 <43751457+Lubin1611@users.noreply.github.com>
2018-11-20 14:28:10 +0100

    Compte a rebous v3

--- java.js
+++ java.js
@@ -1,44 +1,41 @@
 var seconds = document.getElementById("seconds");
 var minutes = document.getElementById("minutes");
-var testSec = 60;
-var testMin = 0;
+var heure = document.getElementById("heures")
+var testSec = 20;
+var testMin = 1;
+var testHeure = 0;
 var time;
 
-var countUp = function() {
+var chrono = function() {
+
+    time = setTimeout(chrono, 1000);
 
 
-    seconds.innerHTML = testSec--;
-    time = setTimeout(countUp, 1000);
+        if (testMin < 0) {
 
-    if (testSec == -1) {
+                testMin = 59;
+                testHeure--;
 
-        //clearTimeout(time);
-        testSec = 60;
-    if (testMin == -1) {
+            }
 
-        clearTimeout(time);
+            else if (testSec < 0)  {
+
+            testSec = 60;
+            testMin--;
         }
 
-    }
+            else if (testHeure == 0 && testMin == 0 && testSec == 0) {
+
+                clearTimeout(time);
+        }
+
+    seconds.innerHTML = testSec-- + " sec";
+    minutes.innerHTML = testMin + " minutes";
+    heure.innerHTML = testHeure + " H";
 
 };
 
-countUp();
 
-
-
-var minPasse = function () {
-
-    minutes.innerHTML = testMin--;
-    time = setTimeout(minPasse, 60000);
-
-    if(testMin == -1) {
-
-        clearTimeout(time);
-    }
-};
-
-minPasse();
 
         var stopCountUp = function () {
 
@@ -48,10 +45,62 @@ minPasse();
         };
 
 
+        var stopButton = document.getElementById("stop-button");
+        stopButton.addEventListener("click", stopCountUp);
+
+        var playButton = document.getElementById("play-buuton");
+        playButton.addEventListener("click", chrono);
 
 
-var stopButton = document.getElementById("stop-button");
-stopButton.addEventListener("click", stopCountUp);
 
-var playButton = document.getElementById("play-buuton");
-playButton.addEventListener("click", countUp);
+        function rezet () {
+
+             testSec = 0;
+             testMin = 0;
+             testHeure = 0;
+
+
+        }
+    function soumettre () {
+
+
+        var entreeHeure = document.getElementById("heure").value;
+        var entreeMin = document.getElementById("minute").value;
+        var entreeSec = document.getElementById("seconde").value;
+
+        if(entreeHeure) {
+            testHeure = entreeHeure;
+        }
+        else
+        {
+            testHeure = 0;
+        }
+
+        if (entreeMin) {
+            testMin = entreeMin;
+        }
+        else {
+
+            testMin = 0;
+        }
+
+        if(entreeSec) {
+            testSec = entreeSec;
+        }
+        else {
+
+            testSec = 0;
+        }
+
+
+       /** if (entreeHeure) {
+
+
+            preventDefault(soumettre());
+        }
+        else
+        {
+            alert('rien');
+        }
+*/
+    }
